821b8a1f42890fd1f18f423a7a2143e57bd42488
tatarenkoigor <tatarenkoigor@gmail.com>
2017-06-30 18:35:11 +0300

    wip

--- pcache.php
+++ pcache.php
@@ -9,12 +9,14 @@ for ($i = 0; $i < $length; $i++) {
 
     $key = $keyPrefix . $i;
 
-    if (false === pcache_set($key, $i, 0)) {
+    if (false === pcache_set($key, $i, $i)) {
         throw new \RuntimeException(var_export(pcache_info(), true));
     }
     var_dump(pcache_get($key));
     //pcache_del($key);
 }
+
+sleep(9);
 
 var_dump(pcache_search('key'));
 
